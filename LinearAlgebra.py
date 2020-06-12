baec2fe5023afd0a34579f603afc3d75cbb06aa9
Vatsal Mistry <vatsalmistry4796@gmail.com>
2020-06-11 20:22:26 -0500

    working add/subtract and scale elements

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -16,12 +16,44 @@ def crossProduct(vector1, vector2):
         k = vector1[0]*vector2[1] - vector2[0]*vector1[1]
         return [i,-j,k]
 
-
 def isOrthogonal(vector1,vector2):
     if dotProduct(vector1,vector2)==0:
         return True
     return False
 
-v1 = [0,0,0]
-v2 = [1,1,1]
-print(isOrthogonal(v1,v2))
+def add(vector1,vector2):
+    newVector = []
+    if len(vector1) != len(vector2):
+        return 'invalid arguments'
+    else:
+        for x in range(len(vector1)):
+            newVector.append(vector1[x]+vector2[x])
+        return newVector
+
+def subtract(vector1,vector2):
+    newVector = []
+    if len(vector1) != len(vector2):
+        return 'invalid arguments'
+    else:
+        for x in range(len(vector1)):
+            newVector.append(vector1[x]-vector2[x])
+        return newVector
+
+def scaleElements(matrix,scalar):
+    if isinstance(matrix[0],list):
+        print("AAAAAAAAA")
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                matrix[row][col] = matrix[row][col]*scalar
+    else:
+        print("BBBBBBBBBBBBB")
+        for x in range(len(matrix)):
+            matrix[x] = matrix[x]*scalar
+
+matrix = [[0,4,6],[1,3,4]]
+scalar = 3
+v2 = [1,5,8]
+scaleElements(matrix,3)
+print(matrix)   
+
+
